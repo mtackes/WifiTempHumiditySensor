14de34041ea9f61441059b5df6bbdc1febcb83a6
mtackes <michaeltackes@gmail.com>
2015-08-10 01:27:30 -0500

    Removing test code.

--- HonorsProjectArduino/HonorsProjectArduino.ino
+++ HonorsProjectArduino/HonorsProjectArduino.ino
@@ -3,15 +3,8 @@
 #include "SFE_CC3000_Client.h"
 #include "DHT.h"
 #include "Indicator.h"
-#include "TestThing.h"
 
 void setup() {
-  Indicator ind(9);
-  ind.blink(4, SHORT);
-  SFE_CC3000 wifiThing(2, 7, 10);
-  TestThing testThing(wifiThing);
-  
-  testThing.connectAll(21);
 }
 
 void loop() {

ac4aa060274ac9fa67a204971bed86cb4c42b9fb
mtackes <michaeltackes@gmail.com>
2015-08-13 04:25:33 -0500

    Creating simple multi-client TCP 'chat' server for testing.

---
+++ server/tcpMultiEcho.js
@@ -0,0 +1,28 @@
+var net = require("net");
+
+var clients = {};
+
+function clientConnected(socket) {
+    var existingSocket = clients[socket.remoteAddress];
+    
+    if (existingSocket) { existingSocket.end("closing duplicate connection"); }
+    
+    clients[socket.remoteAddress] = socket;
+    
+    socket.setEncoding("utf8");
+    socket.on("data", socketReceivedData);
+}
+
+function socketReceivedData(data) {
+    for (address in clients) {
+        clients[address].write(address + ": " + data.trim() + "\r\n");
+    }
+}
+
+function removeThisClient() {
+    delete clients[this.remoteAddress];
+}
+
+var tcpServer = net.createServer(clientConnected);
+    
+tcpServer.listen(9487);

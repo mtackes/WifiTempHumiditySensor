13bd697c48cd9a78f7a589013b7b6294534809fd
mtackes <michaeltackes@gmail.com>
2015-08-20 12:30:54 -0500

    Adding and modifying constants.
    Having the wifi interrupt pin and DHT pin flipped makes for a slightly
    nicer jumper layout on the UNO.

--- HonorsProjectArduino/Configuration.h
+++ HonorsProjectArduino/Configuration.h
@@ -13,12 +13,15 @@
 #define LED_STATUS_PIN  6
 #define LED_ERROR_PIN   5
 
-#define DHT_PIN         3
+#define DHT_PIN         2
 #define DHT_TYPE        DHT22
 
-#define WIFI_INTERRUPT  2
+#define WIFI_INTERRUPT  3
 #define WIFI_ENABLE     7
 #define WIFI_CHIPSELECT 10
+
+#define SERVER_ADDRESS  IPAddress(192, 168, 0, 100)
+#define SERVER_PORT     9487
 
 
 // Private Configration info not included in repo

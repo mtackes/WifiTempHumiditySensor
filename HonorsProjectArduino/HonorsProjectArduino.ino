95a611726d26976d545b34e38ee31fca661e512d
mtackes <michaeltackes@gmail.com>
2015-08-25 15:13:06 -0500

    Adding simple setup.

--- HonorsProjectArduino/HonorsProjectArduino.ino
+++ HonorsProjectArduino/HonorsProjectArduino.ino
@@ -6,7 +6,26 @@
 #include "Indicator.h"
 #include "WifiSensor.h"
 
+WifiSensor module;
+Indicator status(LED_STATUS_PIN);
+Indicator error(LED_ERROR_PIN);
+
 void setup() {
+    if (!module.init()) {
+        error.blink(2, MEDIUM, true);
+    }
+    
+    status.blink(1, SHORT);
+    
+    if (!module.connectToWifi(30000)) {
+        error.blink(3, MEDIUM, true);
+    }
+    
+    status.blink(2, SHORT);
+    
+    if (!module.connectToServer()) {
+        error.blink(4, SHORT, true);
+    }
 }
 
 void loop() {

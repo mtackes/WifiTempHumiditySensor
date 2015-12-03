f8cb0a4fd96f25402707d2209d8dd35a550bb1a1
mtackes <michaeltackes@gmail.com>
2015-12-02 22:48:09 -0600

    Majorly reworking tcp data server.

--- server/dataInputServer.js
+++ server/dataInputServer.js
@@ -1,89 +1,133 @@
 var net = require("net");
-var mongo = require("mongodb").MongoClient;
 
-var dataPointFormat = /([0-9a-f]{12});(\d{1,3}\.\d);(\d{1,2}\.\d)/i;
+// Id is 12 hexadecimal digits
+// Temperature is in the range of -40.0 and 80.0 degrees celcius
+// Humidity is between 0.0 and 99.9 percent
+// Will optionally match on the numbers without the decimal/digit -> dd(.d?)?
+var dataPointFormat = /([0-9a-f]{12});(-?\d{1,2}(?:\.\d?)?);(\d{1,2}(?:\.\d?)?)/i;
+var firstConnectFormat = /;([0-9a-f]{12});/i;
 
-module.exports = function(mongoURL, tcpPortNumber) {
-    mongo.connect(mongoURL, function(err, db) {
-        if (err) {
-            console.log(err);
-            process.exit(2);
-        }
+
+function tcpServerUsingMongoCollection(mongoCollection) {
+    
+    function clientConnected(socket) {
+        var connectionDateTime = new Date();
         
-        console.log("Database Connected.");
+        var clientAddress = socket.remoteAddress;
         
-        var tcpServer = net.createServer(function(socket) {
-            clientConnected(socket, db);
+        // console.log("new connection at " + clientAddress);
+            
+        socket.setEncoding("utf8");
+        
+        // MARK: - Setup socket events
+        
+        var dataCollector = newDataCollector();
+        socket.on("data", dataCollector.collect);
+        
+        socket.setTimeout(20000, function(){
+            console.log("socket timeout event (one time) on remote: " + this.remoteAddress);
+            this.end();
         });
         
-        tcpServer.listen(tcpPortNumber);
-    });
-};
-
-function clientConnected(socket, db) {
-    var clientAddress = socket.remoteAddress;
-    
-    console.log("new connection at " + clientAddress);
+        socket.on("close", closeHandlerForCollectorAtDateWithMongoCollection(
+            dataCollector,
+            connectionDateTime,
+            mongoCollection
+        ));
         
-    socket.setEncoding("utf8");
+        socket.on("end", function(){
+            console.log("end socket");
+            socket.destroy();
+        });
+        
+        socket.on("error", function(error) {
+            console.log("\n\n*********************\n\n");
+            console.log(this);
+            console.log("\n\n*********************\n\n");
+            console.log(error);
+            console.log("\n\n*********************\n\n");
+        });
+    }
     
-    socket.on("data", function(data) {
-        socketReceivedData.bind(this)(data, db);
-    });
-    
-    // socket.on("close", function(){removeThisClient(this, socket.remoteAddress)});
-    // socket.on("end", removeThisClient.bind(null, socket, socket.remoteAddress));
-    // socket.setKeepAlive(true, 10000);
-    
-    socket.setTimeout(20000, function(){
-        console.log("socket timeout event (one time)");
-        this.end();
-    });
-    socket.on("close", function(){
-        console.log("close socket");
-    });
-    socket.on("end", function(){
-        console.log("end socket");
-        socket.destroy();
-    });
-    socket.on("error", function(error) {
-        console.log("\n\n*********************\n\n");
-        console.log(this);
-        console.log("\n\n*********************\n\n");
-        console.log(error);
-        console.log("\n\n*********************\n\n");
-    });
+    return clientConnected
 }
 
-function socketReceivedData(data, db) {
-    var dateReceived = new Date();
-    var dataMatch = data.match(dataPointFormat);
+
+function newDataCollector() {
+    var collector = Object.create(null);
+    collector.data = "";
+    
+    var collectorFunction = function(data) {
+        this.data += data;
+    }
+    collector.collect = collectorFunction.bind(collector);
+    
+    return collector;
+}
+
+function closeHandlerForCollectorAtDateWithMongoCollection(dataCollector, date, mongoCollection) {
+    return function() {
+        var mongoData = mongoDataFromRawInputs(
+            dataCollector.data,
+            date,
+            this.remoteAddress
+        );
+        
+        var firstConnectMatch;
+        
+        if (mongoData) {
+            console.log("-");
+            console.log(mongoData);
+            insertDataInMongoCollection(mongoData, mongoCollection);
+            console.log("-");
+        } else if (firstConnectMatch = dataCollector.data.match(firstConnectFormat)) {
+            console.log("\n---------");
+            console.log("First connection from id: " + firstConnectMatch[1]);
+            console.log("---------\n");
+        } else {
+            console.log("\n*********");
+            console.log("Improperly formatted data received:");
+            console.log(dataCollector.data);
+            console.log("*********\n");
+        }
+        
+        console.log("close socket");
+    }
+
+}
+
+function mongoDataFromRawInputs(rawData, dateReceived, remoteAddress) {
+    var dataMatch = rawData.match(dataPointFormat);
     
     if (!dataMatch) {
-        console.log("Error: data does not match format.");
-        console.log("Data: " + data);
-        console.log("Sent from: " + this.remoteAddress);
-        
-        return;
+        return null;
     }
     
     var hardwareId = dataMatch[1];
     var temperature = parseFloat(dataMatch[2]);
     var humidity = parseFloat(dataMatch[3]);
     
-    db.collection("data").insertOne({
+    return {
         "date": dateReceived,
-        "remoteAddress": this.remoteAddress,
+        "remoteAddress": remoteAddress,
         "hardwareId": hardwareId,
         "temperature": temperature,
         "humidity": humidity
-    }, function(err, result) {
+    }
+}
+
+function insertDataInMongoCollection(data, mongoCollection) {
+    mongoCollection.insertOne(data, function(err, result) {
         if (err) {
-            console.log("error inserting:");
+            console.log("error inserting data:");
             console.log(err);
         } else {
-            console.log(result);
+            console.log(result.result);
         }
     });
-    console.log("[" + this.remoteAddress + "]: " + data);
 }
+
+
+module.exports = {
+    collectionDataSource: tcpServerUsingMongoCollection
+};
